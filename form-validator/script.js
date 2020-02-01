dc6c2a366516f80db3a011a97515f190ef21c65e
Наталья Дронова <natal1@Natashas-MBP.modem>
2020-02-01 14:18:58 +1000

    added validation with if-statements

---
+++ form-validator/script.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// CMD+D - selecting next instance
+// Option + shift + D - copy-paste code below
+
+// Functions
+
+// show error outline
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className += " error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show success outline
+function showSuccess(input, message) {
+  const formControl = input.parentElement;
+  formControl.className += " success";
+}
+
+// check if email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Event listeners
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (username.value == "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value == "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Valid email is required");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value == "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value == "") {
+    showError(password2, "Password 2 is required");
+  } else {
+    showSuccess(password2);
+  }
+});
