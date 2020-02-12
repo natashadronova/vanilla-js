4798b1501b8c0c0aebc81f4a8cdc93461d221f29
natashadronova <ndronova04@gmail.com>
2020-02-13 08:27:43 +1000

    script.js added

--- exchange-rate-calc/script.js
+++ exchange-rate-calc/script.js
@@ -3,32 +3,40 @@ const currencyOptionsOne = document.getElementById("currency-one");
 const currencyOptionsTwo = document.getElementById("currency-two");
 // Fetch amounts
 const amountOne = document.getElementById("amount-one");
-const amountTwo = document.getElementById("amount-two"); 
+const amountTwo = document.getElementById("amount-two");
 // rate
-const rate = document.getElementById("rate");
+const rateEl = document.getElementById("rate");
 // swap button
 const button = document.getElementById("swap");
 
+calculate()
 
 // FUNCTIONS
 // fetch exchange rates and update the DOM
 function calculate() {
     const currencyOne = currencyOptionsOne.value
     const currencyTwo = currencyOptionsTwo.value
-    console.log(currencyOne,currencyTwo)
-     
+
     fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
             const rate = data.rates[currencyTwo]
-            console.log(rate)
+            rateEl.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`
+
+            amountTwo.value = (amountOne.value * rate).toFixed(2)
         })
+
 }
 
 //  EVENT LISTENERS
-currencyOptionsOne.addEventListener("change", calculate) 
-currencyOptionsTwo.addEventListener("change", calculate) 
-amountOne.addEventListener("input", calculate) 
+currencyOptionsOne.addEventListener("change", calculate)
+currencyOptionsTwo.addEventListener("change", calculate)
+amountOne.addEventListener("input", calculate)
 amountTwo.addEventListener("input", calculate)
-button.addEventListener("click", calculate)
+button.addEventListener("click", () => {
+    const temp = currencyOptionsOne.value;
+    currencyOptionsOne.value = currencyOptionsTwo.value;
+    currencyOptionsTwo.value = temp;
+
+    calculate();
+})
